b5c23e5edc6df1fcae679364cc969920e6feb899
Christopher Terrano <mterrano1@yahoo.com>
2021-11-12 18:42:22 -0600

    commit tree 2eed02b60417b8f4ddac50948be7bb0242ea1ad0

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution here
+/*function findMatching(arrayOfDrivers, name){
+    let newDrivers = [];
+
+    for (const driver of arrayOfDrivers) {
+        if (driver.toUpperCase() === name.toUpperCase()){
+            newDrivers.push(driver);
+        }
+    }
+    return newDrivers;
+}
+*/
+
+function findMatching(arrayOfDrivers, name){
+    return arrayOfDrivers.filter(function (driver){
+        return driver.toUpperCase() === name.toUpperCase();
+    })  
+}
+
+function fuzzyMatch(arrayOfDrivers, letters){
+    let lettersLength = letters.length;
+    return arrayOfDrivers.filter(function (driver){
+        return driver.substring(0, lettersLength) === letters;
+    })
+}
+
+function matchName(driverObjects, driverName){
+    return driverObjects.filter(function (driver){
+        return driver.name === driverName;
+    })
+}
